e5ecad3a3022afc124740857c3dff3391abbda42
Jaydeep Makwana <jaydeep.makwana@bypeopletechnologies.com>
2024-03-01 16:49:53 +0530

    update

--- React_Essentials_Components/src/App.jsx
+++ React_Essentials_Components/src/App.jsx
@@ -1,5 +1,5 @@
-import Header from "./components/Header";
-import CoreConcept from "./components/CoreConcept";
+import Header from "./components/Header/Header";
+import CoreConcept from "./components/CoreConcept/CoreConcept";
 import { CORE_CONCEPTS } from "./data";
 
 export default function App() {
