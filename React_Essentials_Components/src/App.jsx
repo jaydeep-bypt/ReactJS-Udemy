3a7ecb2620f95f5bc9ea8e6e6554c61d71c6bee3
Jaydeep Makwana <jaydeep.makwana@bypeopletechnologies.com>
2024-03-01 16:32:52 +0530

    final

---
+++ React_Essentials_Components/src/App.jsx
@@ -0,0 +1,22 @@
+import Header from "./components/Header";
+import CoreConcept from "./components/CoreConcept";
+import { CORE_CONCEPTS } from "./data";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concept</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
