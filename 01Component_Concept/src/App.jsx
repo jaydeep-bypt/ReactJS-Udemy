b39157206c7efe845fe6c1d8be0fa2e640d169af
Jaydeep Makwana <jaydeep.makwana@bypeopletechnologies.com>
2024-03-01 12:38:19 +0530

    first component concept

---
+++ 01Component_Concept/src/App.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import reactImg from "./assets/react-core-concepts.png";
+
+const reactDescription = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function Header() {
+  const description = reactDescription[genRandomInt(2)];
+
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
+function coreConcept() {
+  return (
+    <li>
+      <img src="..." alt="..." />
+      <h3>TITLE</h3>
+      <p>DESCRIPTION</p>
+    </li>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <main>
+        <Header />
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
