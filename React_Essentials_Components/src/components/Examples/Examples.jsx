2366ab3a89145cc53882023468817fb4270b8a6c
Jaydeep Makwana <jaydeep.makwana@bypeopletechnologies.com>
2024-03-04 11:29:00 +0530

    final

--- React_Essentials_Components/src/components/Examples/Examples.jsx
+++ React_Essentials_Components/src/components/Examples/Examples.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import TabButton from "../TabButton/TabButton";
 import { EXAMPLES } from "../../data";
+import Section from "../Section/Section";
 import "./Examples.css";
 
 function Examples() {
@@ -24,30 +25,29 @@ function Examples() {
     );
   }
   return (
-    <section id="examples">
-      <h2>Examples</h2>
+    <Section title="Examples" id="examples">
       <menu>
         <TabButton
           isSelected={selectedTopic === "components"}
-          onSelect={() => handleSelect("components")}
+          onClick={() => handleSelect("components")}
         >
           Components
         </TabButton>
         <TabButton
           isSelected={selectedTopic === "jsx"}
-          onSelect={() => handleSelect("jsx")}
+          onClick={() => handleSelect("jsx")}
         >
           JSX
         </TabButton>
         <TabButton
           isSelected={selectedTopic === "props"}
-          onSelect={() => handleSelect("props")}
+          onClick={() => handleSelect("props")}
         >
           Props
         </TabButton>
         <TabButton
           isSelected={selectedTopic === "state"}
-          onSelect={() => handleSelect("state")}
+          onClick={() => handleSelect("state")}
         >
           State
         </TabButton>
@@ -63,7 +63,7 @@ function Examples() {
       </div>
     )} */}
       {tabContent}
-    </section>
+    </Section>
   );
 }
 
