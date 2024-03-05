813af5367d351a5afdaf977e0498ec4df2649c10
Jaydeep Makwana <jaydeep.makwana@bypeopletechnologies.com>
2024-03-05 12:32:22 +0530

    first

---
+++ Project_Management/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import ProjectSidebar from "./ProjectSidebar/ProjectSidebar";
+
+function App() {
+  return (
+    <main>
+      <ProjectSidebar />;
+    </main>
+  );
+}
+
+export default App;
---
+++ Project_Management/src/ProjectSidebar/ProjectSidebar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ProjectSidebar() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
