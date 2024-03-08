55efd01504c97ee394f230ba2eda5ae6428dfffa
Jaydeep Makwana <jaydeep.makwana@bypeopletechnologies.com>
2024-03-08 10:51:21 +0530

    second commit

--- Food_Order_App/src/components/Header.jsx
+++ Food_Order_App/src/components/Header.jsx
@@ -1,17 +1,33 @@
-import imgLogo from "../assets/logo.jpg";
+import { useContext } from "react";
+
+import Button from "./UI/Button.jsx";
+import logoImg from "../assets/logo.jpg";
+import CartContext from "../store/CartContext.jsx";
+import UserProgressContext from "../store/UserProgressContext.jsx";
 
 export default function Header() {
+  const cartCtx = useContext(CartContext);
+  const userProgressCtx = useContext(UserProgressContext);
+
+  const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
+    return totalNumberOfItems + item.quantity;
+  }, 0);
+
+  function handleShowCart() {
+    userProgressCtx.showCart();
+  }
+
   return (
-    <>
-      <header id="main-header">
-        <div id="title">
-          <img src={imgLogo} alt="FoodImage" />
-          <h1>ReactFood</h1>
-        </div>
-        <nav>
-          <button>Cart(0)</button>
-        </nav>
-      </header>
-    </>
+    <header id="main-header">
+      <div id="title">
+        <img src={logoImg} alt="A restaurant" />
+        <h1>ReactFood</h1>
+      </div>
+      <nav>
+        <Button textOnly onClick={handleShowCart}>
+          Cart ({totalCartItems})
+        </Button>
+      </nav>
+    </header>
   );
 }
